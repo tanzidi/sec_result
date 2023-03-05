16ef7058ed91b663d8e72184983e1d6761ed0bcf
tanzidi <tanzidishoumik@gmail.com>
2023-03-06 00:52:58 +0600

    first-commit

--- django/sec_result/sec_result/settings.py
+++ django/sec_result/sec_result/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
@@ -119,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'sec_result/static'
+# STATIC_ROOT = 'sec_result/static'
+STATIC_ROOT = os.path.join(BASE_DIR, '/sec_result/static/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
